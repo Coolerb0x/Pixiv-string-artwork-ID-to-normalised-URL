dee3942deca741a55b5e264c2fefc9c2a0a969b9
Coolerb0x <coolerbox1992@gmail.com>
2025-11-04 14:34:08 +0400

    Update Hydrus Pixiv filename to link.py
    added comment

--- Hydrus Pixiv filename to link.py
+++ Hydrus Pixiv filename to link.py
@@ -4,7 +4,8 @@ with open('input.txt', 'r', encoding='utf-8') as f_in, \
      open('output.txt', 'w', encoding='utf-8') as f_out:
     
     for line in f_in:
-        # Ищем число перед _p0 в конце строки
+        # Ищем число перед _p0 в конце строки\Searching numbers before _p0 at the end of the string
         match = re.search(r'(\d+)_p\d+$', line)
         if match:
             f_out.write(f"https://www.pixiv.net/en/artworks/{match.group(1)}\n")
+
